bce16d9868fbcd949949531964ccf0fd0713b9c9
Paul Woods <mr.paul.woods@gmail.com>
2024-06-01 23:24:55 -0500

    removing old project

--- lib/main.ts
+++
@@ -1,8 +0,0 @@
-export function helloAnything(thing: string): string {
-    return `Hello ${thing}!`
-}
-
-export {Stat} from './components/Stat/Stat.tsx'
-export {StatRoot} from './components/StatRoot'
-export {StatValue} from './components/StatValue'
-export {StatUnit} from './components/StatUnit'
