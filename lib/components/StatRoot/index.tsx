1eb32c840a8f0a1bc4aacddb5af4d7041d293f3f
Paul Woods <mr.paul.woods@gmail.com>
2024-06-01 22:12:32 -0500

    feat: 0.0.5

--- lib/components/StatRoot/index.tsx
+++ lib/components/StatRoot/index.tsx
@@ -4,18 +4,23 @@ import {StatOwnerState} from "../../types.tsx";
 export const StatRoot = styled('div', {
     name: 'MuiStat',
     slot: 'root'
-})<{ ownerState: StatOwnerState }>(({theme, ownerState}) => ({
-    display: "flex",
-    flexDirection: "column",
-    gap: theme.spacing(0.5),
-    padding: theme.spacing(3, 4),
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: theme.shape.borderRadius,
-    boxShadow: theme.shadows[2],
-    letterSpacing: '-0.025em',
-    fontWeight: 600,
-    ...ownerState.variant === 'outlined' && {
-        border: `2px solid ${theme.palette.divider}`
+})<{ ownerState: StatOwnerState }>(({theme, ownerState}) => {
+
+    console.log('StatRoot palette mode', theme.palette.mode)
+
+    return {
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.spacing(0.5),
+        padding: theme.spacing(3, 4),
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: theme.shape.borderRadius,
+        boxShadow: theme.shadows[2],
+        letterSpacing: '-0.025em',
+        fontWeight: 600,
+        ...ownerState.variant === 'outlined' && {
+            border: `2px solid ${theme.palette.divider}`
+        }
     }
-}))
+})
 
